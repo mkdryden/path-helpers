49781fab24f2ccadce53dd07a5b89dd1235337c6
Christian Fobel <christian@fobel.net>
2017-01-26 12:23:16 -0500

    Use setuptools for packaging

--- setup.py
+++ setup.py
@@ -1,38 +1,23 @@
 #!/usr/bin/env python
+import sys
 
-import distutils.core
+import setuptools as st
 
-try:
-    from distutils.command.build_py import build_py_2to3 as build_py
-except ImportError:
-    from distutils.command.build_py import build_py
+sys.path.insert(0, '.')
 import version
 
-# Setup script for path
-
-kw = {
-    'name': 'path_helpers',
-    'version': version.getVersion(),
-    'description': 'Helper class and functions for working with file path',
-    'author': 'Christian Fobel',
-    'author_email': 'christian@fobel.net',
-    'url': 'http://github.com/cfobel/path_helpers',
-    'license': 'MIT License',
-    'packages': ['path_helpers'],
-    'cmdclass': dict(build_py=build_py)
-}
-
-
-# If we're running Python 2.3, add extra information
-if hasattr(distutils.core, 'setup_keywords'):
-    if 'classifiers' in distutils.core.setup_keywords:
-        kw['classifiers'] = [
-            'Development Status :: 5 - Production/Stable',
-            'License :: OSI Approved :: MIT License',
-            'Intended Audience :: Developers',
-            'Operating System :: OS Independent',
-            'Programming Language :: Python',
-            'Topic :: Software Development :: Libraries :: Python Modules']
-
-
-distutils.core.setup(**kw)
+st.setup(name='path_helpers',
+         version=version.getVersion(),
+         description='Helper class and functions for working with file path',
+         author='Christian Fobel',
+         author_email='christian@fobel.net',
+         url='http://github.com/cfobel/path_helpers',
+         license='MIT License',
+         packages=['path_helpers'],
+         classifiers=
+         ['Development Status :: 5 - Production/Stable',
+          'License :: OSI Approved :: MIT License',
+          'Intended Audience :: Developers',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Topic :: Software Development :: Libraries :: Python Modules'])
